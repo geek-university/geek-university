9774076b32f65290b8b4816668b9c324317961e6
gg <gg@ggbook.local>
2013-03-20 13:45:55 +0400

    Change serialize type to YAML

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,5 +1,5 @@
 class Material < ActiveRecord::Base
   belongs_to :section
   attr_accessible :name, :position, :section_id, :study_material_type, :data
-  serialize :data, JSON
+  serialize :data
 end
