291d9609e1e128e0534529c7f2901149b7ddcae6
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-03-20 06:14:20 +0400

    improve first time user usability

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @courses = Course.all
     if current_user
       @courses = Course.joins(:course_users).where(course_users: { user_id:  current_user.id })
+      if @courses.empty?
+        redirect_to courses_path
+      end
+    else
+      @courses = Course.all
     end
   end
 
